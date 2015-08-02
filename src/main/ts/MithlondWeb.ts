51e0e292096b5d063488b89c5db6b25bccf4c0a6
Lennart Jörelid <lj@jguru.se>
2015-08-02 18:11:34 +0200

    Added some skeleton website documents.

---
+++ src/main/ts/MithlondWeb.ts
@@ -0,0 +1,101 @@
+(function () {
+    'use strict';
+
+    // 1) Instantiate all dependency module instances.
+    angular.module('people', []);
+
+    // 2) Create application instance; inject dependency module references.
+    var app = angular.module('MithlondWebApp', ['ngRoute', 'ngSanitize', 'mgcrea.ngStrap', 'people']);
+
+    // 3) Configure the application.
+    //    ("Constructor" call, invoked before scopes are defined/injected.)
+    app.config(['$routeProvider', function ($routeProvider) {
+
+        // Define routes
+        $routeProvider.when('/login', {
+            templateUrl: 'app/modules/people/peopleView.html',
+            controller: 'peopleController'
+        }).when('/peopleDetails/:jpaId', {
+            templateUrl: 'app/modules/people/peopleDetailView.html',
+            controller: 'peopleController'
+        }).otherwise({
+            redirectTo: '/people'
+        });
+    }
+    ]);
+
+    // 4) Initialize the application's rootScope state.
+    //    ("Constructor" call, invoked *after* scopes are defined/injected.)
+    app.run(['$http', '$rootScope', function ($http, $rootScope) {
+
+        /**
+         * Singleton function for creating a Theme structure.
+         *
+         * @param name The name of the returned Theme. Must not contain whitespace, and should be a
+         * legal CSS class name. The name of the selected theme is added as a CSS class to the 'body' element.
+         * @param structureID The optional id of the structure path used for including header and footer markup files.
+         * Must not contain whitespace, and defaults to 'standard'
+         */
+        $rootScope.createTheme = new function (name, structureID) {
+
+            /**
+             * The active theme's name.
+             */
+            this.name = name;
+
+            /**
+             * The structureID of the themes. Defaults to 'standard' if not supplied.
+             */
+            this.structureID = (typeof structureID === 'undefined') ? 'standard' : structureID;
+
+            /**
+             * Retrieves a path to the theme's Footer file, which should be an include-able HTML file.
+             * @returns {string} The path to the theme's Footer file, which should be an include-able HTML file.
+             */
+            this.footer = function () {
+                return 'app/shared/theme/' + this.structureID + '/footer.html';
+            };
+
+            /**
+             * Retrieves a path to the theme's top NavBar file, which should be an include-able HTML file.
+             * @returns {string} The path to the theme's top NavBar file, which should be an include-able HTML file.
+             */
+            this.topNavBar = function () {
+                return 'app/shared/theme/' + this.structureID + '/topNavbar.html';
+            }
+        };
+
+        /**
+         * Sets the theme with the supplied themeName as the active/selected theme.
+         *
+         * @param themeName The name of the theme to select.
+         */
+        $rootScope.setTheme = new function (themeName) {
+
+            // Check sanity
+            if (typeof themeName === 'string' || themeName instanceof String) {
+
+                // Find the theme with the supplied name
+                for (var i = 0; i < $rootScope.themes.length; i++) {
+                    if ($rootScope.themes[i].name == themeName) {
+
+                        // Set the new (?) theme
+                        $rootScope.theme = $rootScope.themes[i];
+
+                        // All done.
+                        return;
+                    }
+                }
+            }
+        };
+
+        // Shared state: All available themes.
+        $rootScope.themes = [
+            new $rootScope.createTheme('standard'),
+            new $rootScope.createTheme('mint'),
+            new $rootScope.createTheme('pink')];
+
+        // Shared state: The active theme.
+        $rootScope.setTheme('mint');
+    }]);
+}());
---
+++ src/main/webapp/app/modules/mithlond/module.js
@@ -0,0 +1,51 @@
+angular.module('mithlond')
+        .factory('mithlond', function () {
+
+            // Internal state
+            // TODO: Replace this static data by data retrieved from WebSocket call.
+            var people = [{
+                jpaId: 25,
+                firstName: 'Johannes',
+                lastName: 'Brahms',
+                birthday: '1833-05-07T00:00:00Z'
+            }, {
+                jpaId: 29,
+                firstName: 'Ludvig',
+                lastName: 'van Beethoven',
+                birthday: '1770-12-17T00:00:00Z'
+            }, {
+                jpaId: 382,
+                firstName: 'Antonio',
+                lastName: 'Vivaldi',
+                birthday: '1678-03-04T00:00:00Z'
+            }];
+            var selected = -1;
+
+            // Public accessors
+            return {
+
+                get: function () {
+                    return people;
+                },
+
+                select: function (jpaId) {
+                    selected = jpaId;
+                },
+
+                find: function (index) {
+                    return people[index];
+                }
+            };
+        })
+        .controller('mithlondController', ['$scope', '$routeParams', 'mithlond',
+            function ($scope, $routeParams, mithlond) {
+
+                // Make the members accessible in the scope
+                $scope.people = people.get();
+
+                // If we have detail information, expose it in the scope.
+                if ($routeParams.jpaId != 'Undefined') {
+                    people.select($routeParams.jpaId);
+                    $scope.id = $routeParams.jpaId;
+                }
+            }]);
